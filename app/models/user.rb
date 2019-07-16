b421ccde76bfe723d1a4df47c781687ffd5130d3
msjlwhite <msjlwhite@gmail.com>
2019-07-16 12:31:39 -0500

    Current Version as of July 10, 2019 @ 12:31 PM

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-    has_many :comment, class_name: "comment, foreign_key: "reference_id"
+    has_many :comment, class_name: "comment", foreign_key: "reference_id"
 end
